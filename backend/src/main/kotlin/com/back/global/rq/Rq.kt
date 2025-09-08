b7c5838f2228ca7f026298ec24396aebb1015794
meohin <meohin.meo@gmail.com>
2025-09-08 14:40:52 +0900

    refactor: 쿠키 도메인 설정화

--- backend/src/main/kotlin/com/back/global/rq/Rq.kt
+++ backend/src/main/kotlin/com/back/global/rq/Rq.kt
@@ -1,12 +1,10 @@
 package com.back.global.rq
 
 import com.back.domain.member.member.entity.Member
-import com.back.domain.member.member.entity.MemberProxy
 import com.back.domain.member.member.service.MemberService
-import com.back.domain.post.postUser.entity.PostUser
-import com.back.domain.post.postUser.entity.PostUserProxy
-import com.back.domain.post.postUser.service.PostUserService
+import com.back.global.app.AppConfig // 추가됨
 import com.back.global.security.SecurityUser
+import com.back.standard.extensions.getOrThrow
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -18,7 +16,6 @@ class Rq(
     private val req: HttpServletRequest,
     private val resp: HttpServletResponse,
     private val memberService: MemberService,
-    private val postUserService: PostUserService,
 ) {
     val actor: Member
         get() = SecurityContextHolder
@@ -27,36 +24,15 @@ class Rq(
             ?.principal
             ?.let {
                 if (it is SecurityUser) {
-                    MemberProxy(
-                        it.id,
-                        it.username,
-                        it.nickname,
-                        memberService.getReferenceById(it.id)
-                    )
+                    Member(it.id, it.username, it.nickname)
                 } else {
                     null
                 }
             }
             ?: throw IllegalStateException("인증된 사용자가 없습니다.")
 
-    val postActor: PostUser
-        get() = SecurityContextHolder
-            .getContext()
-            ?.authentication
-            ?.principal
-            ?.let {
-                if (it is SecurityUser) {
-                    PostUserProxy(
-                        it.id,
-                        it.username,
-                        it.nickname,
-                        postUserService.getReferenceById(it.id)
-                    )
-                } else {
-                    null
-                }
-            }
-            ?: throw IllegalStateException("인증된 사용자가 없습니다.")
+    val actorFromDb: Member
+        get() = memberService.findById(actor.id).getOrThrow()
 
     fun getHeader(name: String, defaultValue: String): String {
         return req.getHeader(name) ?: defaultValue
@@ -77,7 +53,7 @@ class Rq(
         val cookie = Cookie(name, value ?: "").apply {
             path = "/"
             isHttpOnly = true
-            domain = "localhost"
+            domain = AppConfig.cookieDomain // 수정됨
             secure = true
             setAttribute("SameSite", "Strict")
             maxAge = if (value.isNullOrBlank()) 0 else 60 * 60 * 24 * 365
