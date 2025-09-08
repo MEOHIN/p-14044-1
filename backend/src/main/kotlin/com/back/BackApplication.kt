e7a8ce8b2198b2da993fb6d4d0f3f16adab8138d
meohin <meohin.meo@gmail.com>
2025-09-08 14:44:38 +0900

    feat: 스프링 캐싱 기능 활성화

--- backend/src/main/kotlin/com/back/BackApplication.kt
+++ backend/src/main/kotlin/com/back/BackApplication.kt
@@ -1,11 +1,14 @@
 package com.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching // 스프링 캐시 활성화(이건 딱히 지금 필요하지는 않음)
 class BackApplication
 
 fun main(args: Array<String>) {
